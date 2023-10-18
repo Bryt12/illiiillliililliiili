e07641c4044e5ceae2fd6ae26aafb97b59e65b3e
Bryson Tang <brysontang@gmail.com>
2023-10-17 21:08:36 -0400

    + expensive ceder

---
+++ expensive_ceder/sketch.js
@@ -0,0 +1,63 @@
+function setup() {
+  createCanvas(400, 400);
+
+  // blendMode(ADD
+}
+
+function draw() {
+  background(0);
+
+  for (var i = 0; i < 22; i++) {
+    mask = createGraphics(400, 400);
+    mask.translate(0, -50);
+    mask.colorMode(HSL, 1);
+    let x = (abs(i) % 5) * 100;
+    let y = (i / 4) * 100;
+    mask.translate(x, y);
+    // mask.background(0)
+    mask.blendMode(ADD);
+    mask.noFill();
+    // mask.noStroke()
+    ctx = mask.canvas.getContext('2d');
+    mask.circle(0, 0, 400 / 3);
+    ctx.clip();
+    eye(mask);
+    image(mask, 0, 0);
+  }
+
+  noLoop();
+}
+
+function eye(mask) {
+  let layers = 100;
+  for (var i = 0; i < layers; i++) {
+    layer(i, mask);
+  }
+}
+
+function layer(lay, mask) {
+  let n = random(20);
+  let n2 = random(10, 40);
+  let r = 300;
+
+  for (var j = 0; j < n2; j++) {
+    mask.rotate((2 * PI) / n2);
+    for (var i = 0; i < n; i++) {
+      mask.stroke(nc(i + lay * 100));
+      let t = map(i, 0, n, 0, 10 * PI);
+      let t2 = t * 1.2;
+      let x = r * cos(t);
+      let y = r * sin(t);
+
+      let x2 = r * cos(t2);
+      let y2 = r * sin(t2);
+
+      mask.line(x, y, x2, y2);
+    }
+  }
+}
+
+function nc(i) {
+  i = i / 50;
+  return color(255 * noise(i), 255 * noise(i + 1), 255 * noise(i + 2), 10);
+}
